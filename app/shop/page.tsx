452c85e06ea758b518f00dad32b203cd6247aec9
devansh <devanshverma193@gmail.com>
2024-10-20 03:40:34 +0530

    shop

---
+++ app/shop/page.tsx
@@ -0,0 +1,7 @@
+import Navbar from "@/components/navbar";
+
+const Shop = () => {
+  return <div>hi there</div>;
+};
+
+export default Shop;
--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -16,7 +16,7 @@ export const Main = () => {
           EXPLORE, INDULGE, AND LET THE &apos;ohh&apos; MOMENTS BEGIN!
         </p>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 mx-auto mt-10">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-2 mx-auto mt-10">
         <div className="col-span-1">
           <Image
             src="/bag1.jpeg"
